ec8e5380cb04aeb8eacf179cc37dfce891a1146e
Gustavo Rondello <gustavorondello99@outlook.com>
2021-12-10 17:00:19 -0300

    Requisito 8 completo

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-window.onload = function (){
-    const button = document.getElementById('criar-tarefa');
-    const input = document.getElementById('texto-tarefa');
-    const listaTarefas = document.getElementById('lista-tarefas');
+window.onload = function () {
+  const button = document.getElementById('criar-tarefa');
+  const input = document.getElementById('texto-tarefa');
+  const listaTarefas = document.getElementById('lista-tarefas');
 
-    function criaTarefa () {
-        const tarefaCriada = document.createElement('li')
-        listaTarefas.appendChild(tarefaCriada);
-        tarefaCriada.innerHTML += input.value;
-        input.value = null;
-        console.log(tarefaCriada)
+  function createTask() {
+    const tarefaCriada = document.createElement('li');
+    tarefaCriada.classList.add('tarefas');
+    listaTarefas.appendChild(tarefaCriada);
+    tarefaCriada.innerHTML += input.value;
+    input.value = null;
+    console.log(tarefaCriada);
+  }
+
+  function selectedTask(event) {
+    const tarefa = document.querySelectorAll('.tarefas');
+    for (let i = 0; i < tarefa.length; i += 1) {
+      const posicaoTarefa = tarefa[i];
+      posicaoTarefa.classList.remove('cinza-claro');
     }
+    event.target.classList.add('cinza-claro');
+  }
 
-
-    button.addEventListener('click', criaTarefa)
-}
+  button.addEventListener('click', createTask);
+  listaTarefas.addEventListener('click', selectedTask);
+};
